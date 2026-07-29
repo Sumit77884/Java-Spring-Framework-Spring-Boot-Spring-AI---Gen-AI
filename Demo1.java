b200b8260aa0f99cc40cb2a13a3afde2985cd52d
Sumit Kumar <sumitsingh1999220@gmail.com>
2026-07-29 20:05:59 +0000

    Add Demo.java and Demo1.java

---
+++ Demo1.java
@@ -0,0 +1,10 @@
+public class Demo1 {
+ public static void main(String [] args){
+String name="navin";
+name =name +" ready";
+System.out.println("Hellow "+name);
+String s1="Navin";
+String s2="Name";
+System.out.println(s1==s2);
+ }   
+}
