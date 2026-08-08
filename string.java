79a8f9a8f925477364aec48129414903a58567de
Sumit Kumar <sumitsingh1999220@gmail.com>
2026-08-08 21:55:44 +0000

    Add String demo

---
+++ string.java
@@ -0,0 +1,10 @@
+public class string {
+    public static void main(string[] args){
+        String name=new string();
+        System.out.println(name);
+        System.out.println(name.hashCode());
+        System.out.println("Hellow "+name);
+        System.out.println(name.concat("reddy"));
+        
+    }
+}
