6ad6e4a936e681de6daf8e9b01c2f9b336f596c3
Sumit Kumar <sumitsingh1999220@gmail.com>
2026-08-12 18:19:11 +0000

    Add mutable and immutable string example

---
+++ mutable_unmutable_string.java
@@ -0,0 +1,7 @@
+public class mutable_unmutable_string {
+    public static void main(String [] args){
+        String name="Navin";
+        name=name+"reddy";
+        System.out.println("Hellow"+name);
+    }
+}
