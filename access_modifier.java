bc5ae67d5a8a3341ec6f1bdb1d343ee1c5c1226a
Sumit Kumar <sumitsingh1999220@gmail.com>
2026-08-13 22:33:55 +0000

    add

---
+++ access_modifier.java
@@ -0,0 +1,31 @@
+class Launch{
+    A obj=new A();
+   // System.out.println(obj.marks);
+}
+class A{
+    int marks=6;
+    // protected marks=6;
+    public void show(){
+
+    }
+}
+class B{
+    int marks;
+}
+class C extends A{
+    public void abc(){
+        System.out.println(marks);
+    }
+}
+public class access_modifier {
+    public static void main(String [] args){
+ A obj=new A();
+ System.out.println(obj.marks);
+ obj.show();
+ B obj1=new B();
+ System.out.println(obj.marks);
+
+
+    }
+    
+}
