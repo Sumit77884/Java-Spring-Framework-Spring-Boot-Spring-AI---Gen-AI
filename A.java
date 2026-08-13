3b5cd3286d94d0f7379d7777924b6e56d858085b
Sumit Kumar <sumitsingh1999220@gmail.com>
2026-08-13 19:28:57 +0000

    add

---
+++ A.java
@@ -0,0 +1,31 @@
+class Calc{
+    public int add(int n1,int n2){
+        return n1+n2;
+    }
+    public int sub(int n1,int n2){
+        return n1-n2;
+    }
+}
+public class AdvCalc extends Calc{
+    
+}
+public class inheritance {
+    public static void main(String [] ags){
+        /*
+        Inheritance:-
+        is, has
+        is is used in inheritance
+        class Calc
+        {
+        add()
+        sub()
+        multi()
+        div()
+        }
+        class AdvCal extends Calc{
+        
+        }
+        
+        */
+    }
+}
